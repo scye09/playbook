0bebeaeca286fea5cd9ded2debde94f09fb56347
Sophia Ye <scye09@gmail.com>
2017-08-03 21:47:08 +0000

    use store plugin to post annotations to db

--- settings.py
+++ settings.py
@@ -9,55 +9,13 @@ DEBUG = True
 ALLOW_UNKNOWN = True
 
 annotationSchema = {
-    #'annotation': {
-    #}
-    '''
-     "id": {
-        'type': 'string'
-     },  # unique id (added by backend)
-     "annotator_schema_version": {
-        'type': 'string'
-     },       # schema version: default v1.0
-     "created": {
-        'type': 'datetime'
-     },   # created datetime in iso8601 format (added by backend)
-     "updated": {
-        'type': 'datetime'
-     },   # updated datetime in iso8601 format (added by backend)
-     "text": {
-        'type': 'string'
-     },                  # content of annotation
-     "quote": {
-        'type': 'string'
-     },   # the annotated text (added by frontend)
-     "uri": {
-        'type': 'string'
-     },              # URI of annotated document (added by frontend)
-      "ranges": {
-        'type': 'list',
-        'schema': {
-            'allow_unknown': True
-        }
-      },                                # list of ranges covered by annotation (usually only one entry)
-        #{
-        #  "start": "/p[69]/span/span",           # (relative) XPath to start element
-        #  "end": "/p[70]/span/span",             # (relative) XPath to end element
-        #  "startOffset": 0,                      # character offset within start element
-        #  "endOffset": 120                       # character offset within end element
-        #}
-      "user": {
-        'type': 'string'
-      },                          # user id of annotation owner (can also be an object with an 'id' property)
-      "consumer": {
-        'type': 'string'
-      }                  # consumer key of backend
-      '''
 }
+
 
 annotations = {
     'schema': annotationSchema,
-    'url': 'annotations/annotations'
 }
+
 
 DOMAIN = {
     'annotations': annotations,
--- templates/test.js
+++ templates/test.js
@@ -1,41 +1,33 @@
-  jQuery(function ($) {
-      var annotation = $(document.body).annotator();
-      /*annotation.annotator("loadAnnotations", {
-      "id": "39fc339cf058bd22176771b3e3187329",
-      "created": "2011-05-24T18:52:08.036814",
-      "updated": "2011-05-26T12:17:05.012544",
-      "text": "This is a comment",
-      "quote": "Lorem",
-      "ranges":
-      [{
-        "start": "/div[1]",
-        "end": "/div[1]",
-        "startOffset": 17,
-        "endOffset": 22
-      }]
-    });*/
-      annotation.annotator('addPlugin', 'Store', {
-          // The endpoint of the store on your server.
-          prefix: '/annotations',
+jQuery(function ($) {
+     var annotation = $(document.body).annotator()
+                                      .annotator('setupPlugins', {}, {
+                                        Auth: false,
+                                        Permissions: false,
+                                        Store: false
+                                      });
+    
+     annotation.annotator('addPlugin', 'Store', {
+         // The endpoint of the store on your server.
+         prefix: '/annotations',
 
-          // Attach the uri of the current page to all annotations to allow search.
-          annotationData: {
-            'uri': 'http://67.207.95.7:5000/test'
+         // Attach the uri of the current page to all annotations to allow search.
+         annotationData: {
+           'uri': 'http://67.207.95.7:5000/test'
+         },
+
+         // This will perform a "search" action when the plugin loads. Will
+         // request the last 20 annotations for the current url.
+         // eg. /store/endpoint/search?limit=20&uri=http://this/document/only
+         loadFromSearch: {
+           'limit': 20,
+          'uri': 'http://67.207.95.7:5000/test'
+        },
+
+         urls: {
+              create:  '/store',
+              update:  '/update/:id',
+              destroy: '/delete/:id',
+              search:  '/search'
           },
-
-          // This will perform a "search" action when the plugin loads. Will
-          // request the last 20 annotations for the current url.
-          // eg. /store/endpoint/search?limit=20&uri=http://this/document/only
-          //loadFromSearch: {
-          //  'limit': 20,
-          //  'uri': 'http://67.207.95.7:5000/test'
-          //},
-
-          urls: {
-               create:  '/annotations',
-               update:  '/update/:id',
-               destroy: '/delete/:id',
-               search:  '/search'
-           },
-        });
-  });
+       });
+ });
