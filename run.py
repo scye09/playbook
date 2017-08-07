23958b9233f5602f890b6ef424f5b308ec594742
Donna <mujinyun@seas.upenn.edu>
2017-08-07 12:22:22 -0400

    Encoded objectID and datetime

--- run.py
+++ run.py
@@ -5,9 +5,19 @@ from flask import jsonify
 from flask_pymongo import PyMongo
 import requests
 from bson.objectid import ObjectId
+from eve.io.base import BaseJSONEncoder
+from datetime import datetime
 
 app = Eve(__name__, template_folder='templates')
 mongo = app.data.driver
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        if isinstance(o, datetime):
+            return o.__str__()
+        return json.JSONEncoder.default(self, o)
 
 def post_annotation_callback(docs):
     for doc in docs:
@@ -27,15 +37,24 @@ def test_js():
 
 @app.route('/annotations/search')
 def search_annotation():
+
     params = dict(request.args.items())
     uri = params['uri']
-    last_slash = uri.rfind('/')
-    base_url = uri[:(last_slash+1)]
-    query_url = base_url + 'annotations?where={"uri": "' + uri + '"}'
-    response = requests.get(query_url)
-    text = response.json()
-    items = text['_items']
-    return jsonify({"total": len(items), "rows": items})
+    f = {'uri': uri}
+    query_results = mongo.db.annotations.find(f)
+    query_items = []
+    for result in query_results:
+        query_items.append(result)
+
+    # query_json = json.dumps(query_items, default=json_util.default)
+
+    # last_slash = uri.rfind('/')
+    # base_url = uri[:(last_slash+1)]
+    # query_url = base_url + 'annotations?where={"uri": "' + uri + '"}'
+    # response = requests.get(query_url)
+    # text = response.json()
+    # items = text['_items']
+    return JSONEncoder().encode({"total": len(query_items), "rows": query_items})
 
 
 if __name__ == '__main__':
