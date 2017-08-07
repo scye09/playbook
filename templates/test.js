4e71fe857439ee4e71fbc50778d4c605544b5c9e
Sophia Ye <scye09@gmail.com>
2017-08-07 19:44:33 +0000

    add bcrypt, fix CRUD methods

--- templates/test.js
+++ templates/test.js
@@ -1,33 +1,42 @@
 jQuery(function ($) {
-     var annotation = $(document.body).annotator()
-                                      .annotator('setupPlugins', {}, {
-                                        Auth: false,
-                                        Permissions: false,
-                                        Store: false
-                                      });
-    
-     annotation.annotator('addPlugin', 'Store', {
-         // The endpoint of the store on your server.
-         prefix: '/annotations',
 
-         // Attach the uri of the current page to all annotations to allow search.
-         annotationData: {
-           'uri': 'http://67.207.95.7:5000/test'
-         },
+    var annotation = $(document.body).annotator();
 
-         // This will perform a "search" action when the plugin loads. Will
-         // request the last 20 annotations for the current url.
-         // eg. /store/endpoint/search?limit=20&uri=http://this/document/only
-         loadFromSearch: {
-           'limit': 20,
-          'uri': 'http://67.207.95.7:5000/test'
+
+    annotation.annotator('addPlugin', 'Permissions', {
+      user: 'admin',
+      permissions: {
+        'read':   [],
+        'update': [],
+        'delete': [],
+        'admin':  ['admin']
+      }
+    });
+
+
+    annotation.annotator('addPlugin', 'Store', {
+        // The endpoint of the store on your server.
+        prefix: '/annotations',
+
+        // Attach the uri of the current page to all annotations to allow search.
+        annotationData: {
+          'uri': window.location.href
         },
 
-         urls: {
-              create:  '/store',
-              update:  '/update/:id',
-              destroy: '/delete/:id',
-              search:  '/search'
-          },
-       });
- });
+        // This will perform a "search" action when the plugin loads. Will
+        // request the last 20 annotations for the current url.
+        // eg. /store/endpoint/search?limit=20&uri=http://this/document/only
+        loadFromSearch: {
+        //  'limit': 20,
+         'uri': window.location.href
+        },
+
+        urls: {
+             create:  '/',
+             update:  '/:id',
+             destroy: '/:id',
+             search:  '/search'
+         },
+      });
+
+});
